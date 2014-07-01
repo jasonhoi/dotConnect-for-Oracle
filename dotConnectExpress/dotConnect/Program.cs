420aff91aecf6ab7f3c1421b0c6421e02b20b347
Jason Hoi Win7 <flashjx1985@gmail.com>
2014-07-01 18:33:53 +0800

    init project

---
+++ dotConnectExpress/dotConnect/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Devart.Data.Oracle;
+
+namespace dotConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OracleConnection con = new OracleConnection();
+            con.ConnectionString = "Server=oracle_db;User Id=web;Password=oracle";
+
+            try
+            {
+                con.Open();
+                Console.WriteLine(con.ServerVersion);
+            } catch(Exception ex) {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
